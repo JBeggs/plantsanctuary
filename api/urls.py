29947a7254b2c720419d85ac66b9328b15a16dc9
Jody <jody.beggs@cullinansystems.co.za>
2023-04-26 14:03:29 +0200

    Updated urls and minor functionality

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import include, path
-from api.views import build, build_html, site_server
+from .views import build, build_html, site_server
 
 admin.autodiscover()
 
--- api/views.py
+++ api/views.py
@@ -5,7 +5,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, redirect
 from os.path import exists
 from django.conf import settings
-from plantsanctuary.api.models import *
+from products.models import *
 
 
 def site_server(request, *args, **kwargs):
@@ -13,6 +13,7 @@ def site_server(request, *args, **kwargs):
     site    = request.GET.get('site')
     section = request.GET.get('section')
     element = request.GET.get('element')
+    product = request.GET.get('product', '')
 
     if section == 'main' and not element:
         default_layout = 'includes/main.html'
@@ -20,6 +21,11 @@ def site_server(request, *args, **kwargs):
         default_layout = 'layouts/main/{}.html'.format(element)
     else:
         default_layout = 'layouts/{}/{}.html'.format(section, element)
+
+    if product != '':
+        products = Product.objects.filter(id=product)
+    else:
+        products = Product.objects.filter(quantity__gt=0)
 
     site_layout = 'layouts/{}/{}-{}.html'.format(section, site, element)
 
@@ -30,7 +36,7 @@ def site_server(request, *args, **kwargs):
     except:
         site_template = default_layout
 
-    return render(request, site_template, {'posts': 'posts', 'site': site})
+    return render(request, site_template, {'products': products, 'site': site})
 
 
 @login_required
