97214dc2b8c921a3f83dd73c0e5a9bc3bea17bc4
Jody <jody.beggs@cullinansystems.co.za>
2023-05-15 14:22:25 +0200

    Started adding all auth configurations and attempting to install djangocms blog

--- contentserver/settings.py
+++ contentserver/settings.py
@@ -63,6 +63,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount.providers.google',
     'allauth.socialaccount.providers.facebook',
     'allauth.socialaccount.providers.linkedin',
+    'allauth.socialaccount.providers.linkedin_oauth2',
     'cms',
     'menus',
     'api',
